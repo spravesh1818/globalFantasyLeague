12c5c0e542ae22a46e000eef43a512f0361885b9
Pravesh Chapagain <spravesh1818@gmail.com>
2020-09-09 12:01:03 +0545

    Refactoring code to remove unnecessary redundant methods from main

--- app/main.py
+++ app/main.py
@@ -20,6 +20,8 @@ from db import database as adb
 
 from usermanagement.users_model import users
 
+from authenticationUtils import authenticate_user, create_access_token
+
 SECRET_KEY = "8ee7b057761c29f8ca0a336f850aa73abf9eeb81c6a5b015c893af74d7e6a948"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
@@ -35,101 +37,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-"""Code for oauth implementation"""
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: Optional[str] = None
-
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-def verify_password(plain_password, hashed_password) -> str:
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password) -> str:
-    return pwd_context.hash(password)
-
-
-async def get_user(username: str) -> UserCreate:
-    query = users.select()
-    user_list = await adb.fetch_all(query)
-    for user in user_list:
-        if user["username"] == username:
-            return UserCreate(**user)
-    return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-
-
-"""authenticating user"""
-
-
-async def authenticate_user(username: str, password: str):
-    user = await get_user(username)
-    if not user:
-        return False
-    if not verify_password(password, user.password):
-        return False
-    return user
-
-
-"""code to create the access token"""
-
-
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-"""getting the current user details"""
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = await get_user(username=token_data.username)
-    if not user:
-        raise credentials_exception
-    return user
-
-
-"""Checking users if they are active or not"""
-
-
-async def get_current_active_user(
-    current_user: User = Depends(get_current_user)
-) -> User:
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
-
-"""Oauth token url.All the logic behind authentication goes here"""
 
 
 @app.post("/token")
@@ -157,20 +64,6 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await adb.disconnect()
-
-
-"""All the routes are stored here"""
-
-
-class NoteIn(BaseModel):
-    text: str
-    completed: bool
-
-
-class Note(BaseModel):
-    id: int
-    text: str
-    completed: bool
 
 
 # @app.get("/notes/", response_model=List[Note])
